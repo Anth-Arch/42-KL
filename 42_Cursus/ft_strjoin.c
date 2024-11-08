be43dcff2c08459a634eeb92e13b7008280405ae
Shiung Ye Kok <shkok@u90z03s03.42kl.edu.my>
2024-11-08 18:56:43 +0800

    ft_strtrim.c

---
+++ 42_Cursus/ft_strjoin.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shkok <shkok@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 08:19:52 by shkok             #+#    #+#             */
+/*   Updated: 2024/11/08 14:26:03 by shkok            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		slen1;
+	int		slen2;
+	int		i;
+	char	*output;
+	char	*temp;
+
+	i = 0;
+	slen1 = ft_strlen(s1);
+	slen2 = ft_strlen(s2);
+	output = (char *)malloc((slen1 + slen2 + 1) * sizeof(char));
+	if (!output)
+		return (NULL);
+	temp = output;
+	while (i++ < slen1)
+		*temp++ = *s1++;
+	i = 0;
+	while (i++ < slen2)
+		*temp++ = *s2++;
+	*temp = '\0';
+	return (output);
+}
+/*
+int main(void)
+{
+	char	*s1 = "ABCDEF";
+	char	*s2 = "DEF";
+	char	*output = ft_strjoin(s1,s2);
+	printf("Output : %s\n", output);
+	free (output);
+	return (0);
+}
+*/
---
+++ 42_Cursus/ft_strtrim.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shkok <shkok@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 14:27:23 by shkok             #+#    #+#             */
+/*   Updated: 2024/11/08 18:55:32 by shkok            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static int	ft_strlen(const char *s)
+{
+	int	len;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+int	ft_strend(char const *s1, char const *set)
+{
+	int	cycle;
+	int	i;
+
+	cycle = 0;
+	i = ft_strlen(s1) - 1;
+	while (set[cycle])
+	{
+		if (s1[i] == set[cycle++])
+		{
+			i--;
+			cycle = 0;
+		}
+	}
+	return (i + 1);
+}
+
+static void	ft_malloc_copy(char *temp, int stop, char const *s1, int i)
+{
+	while (stop--)
+		*temp++ = s1[i++];
+	*temp = '\0';
+	return ;
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	int		cycle;
+	int		stop;
+	char	*output;
+	char	*temp;
+
+	stop = ft_strend(s1, set);
+	i = 0;
+	cycle = 0;
+	while (set[cycle])
+	{
+		if (s1[i] == set[cycle++])
+		{
+			i++;
+			stop--;
+			cycle = 0;
+		}
+	}
+	output = (char *)malloc((stop + 1) * sizeof(char));
+	if (!output)
+		return (NULL);
+	temp = output;
+	ft_malloc_copy(temp, stop, s1, i);
+	return (output);
+}
+/*
+int main(void)
+{
+	char *s1 = "12312A31231ABC23123123";
+	char *set = "123A";
+	char *output = ft_strtrim(s1,set);
+
+	printf("Output : %s\n", output);
+	free (output);
+	return (0);
+}
+*/
